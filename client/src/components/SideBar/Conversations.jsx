210be9d8a2076eb55245e79cd001acec476d8be2
Pritam <manoharipritam@gmail.com>
2024-05-20 12:06:34 +0530

    add working sidebar

--- client/src/components/SideBar/Conversations.jsx
+++ client/src/components/SideBar/Conversations.jsx
@@ -1,14 +1,21 @@
+import useGetConversation from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+    const { loading, conversations } = useGetConversation();
+
     return (
-        <div className='py-2 flex flex-col overflow-auto'>
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+        <div className='py-1 flex flex-col overflow-auto'>
+            {conversations.map((conversation, index) => (
+                <Conversation
+                    key={conversation._id}
+                    conversation={conversation}
+                    emoji={getRandomEmoji()}
+                    lastIndex={index === conversations.length - 1}
+                />
+            ))}
+            {loading ? <span className=" m-auto loading loading-spinner"></span> : null}
         </div>
     );
 };
--- client/src/components/SideBar/Sidebar.jsx
+++ client/src/components/SideBar/Sidebar.jsx
@@ -1,18 +1,15 @@
-import useGetConversation from "../../hooks/useGetConversations";
 import Conversations from "./Conversations";
- import LogOutButton from "./LogOutButton";
- import SearchInput from "./SearchInput";
+import LogOutButton from "./LogOutButton";
+import SearchInput from "./SearchInput";
 
- const Sidebar = () => {
-	const {loading, conversations} = useGetConversation();
-	console.log(conversations);
- 	return (
- 		<div className='border-r border-slate-500 p-4 flex flex-col'>
- 			<SearchInput />
- 			<div className='divider px-3'></div>
- 			<Conversations />
- 			<LogOutButton />
- 		</div>
- 	);
- };
- export default Sidebar;
+const Sidebar = () => {
+	return (
+		<div className='border-r border-slate-500 p-4 flex flex-col'>
+			<SearchInput />
+			<div className='divider px-3'></div>
+			<Conversations />
+			<LogOutButton />
+		</div>
+	);
+};
+export default Sidebar;
