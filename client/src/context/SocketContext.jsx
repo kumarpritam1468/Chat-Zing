fe2f15d3783ccbaa800a4ce3212573f52c6239af
Pritam Kumar Manohari <124861908+kumarpritam1468@users.noreply.github.com>
2024-05-23 12:08:42 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io('http://localhost:5000', {
+            const socket = io('https://chat-zing.onrender.com', {
                 query:{
                     userId: authUser._id
                 }
